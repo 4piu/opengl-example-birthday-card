f3ab0956daef41c0b4a9df8fafa7493616c46ac0
4piu <16668056@users.noreply.github.com>
2020-11-06 01:43:30 +0800

    random floral

--- main.cpp
+++ main.cpp
@@ -7,22 +7,31 @@
 #define APP_WIDTH 720
 #define APP_HEIGHT 960
 
-typedef struct {
-    GLfloat x, y;
-} point;
+#define FLORAL_COUNT 100
+#define FLORAL_MARGIN 10
+#define FLORAL_SIZE_MIN 3
+#define FLORAL_SIZE_MAX 8
 
 typedef struct {
-    point coordinator;
+    GLfloat x, y;
+} Point;
+
+typedef struct {
+    Point coordinator;
     float size;
     float rotation;
     float color[3];
     float alpha;
-    enum shape {
+    enum Shape {
         TRIANGLE, CIRCLE, SQUARE
     } shape;
 } floral;
 
 // function declarations
+
+int random_int(int, int);
+
+float random_float(float, float);
 
 void use_absolute_cs();
 
@@ -40,14 +49,17 @@ void mouse_handler(int, int, int, int);
 
 void motion_handler(int, int);
 
-void draw_floral(floral);
+void init_floral();
+
+void draw_floral(floral *);
 
 void draw_background();
 
 void draw_pointer();
 
 // global vars
-point mouse_pointer = {0, 0};
+Point mouse_pointer = {0, 0};
+floral bk_decorations[FLORAL_COUNT];
 
 int main(int argc, char *argv[]) {
     glutInit(&argc, argv);
@@ -71,7 +83,10 @@ int main(int argc, char *argv[]) {
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-    glutMainLoop();
+    // init global vars
+    init_floral();
+
+    glutMainLoop(); // m4k3 17 r41n
     return 0;
 }
 
@@ -109,7 +124,7 @@ void reshape_handler(int w, int h) {
         new_h = (int) ((float) w / (float) APP_WIDTH * (float) APP_HEIGHT);
     }
     glViewport((w - new_w) / 2, (h - new_h) / 2, new_w, new_h); // set viewport
-    glutReshapeWindow(new_w, new_h);    // force shape window to specified ratio
+    glutReshapeWindow(new_w, new_h);    // force Shape window to specified ratio
 }
 
 void timer_handler(int) {
@@ -127,6 +142,14 @@ void draw_background() {
     glVertex2f(1, 0);
     glVertex2f(0, 0);
     glEnd();
+
+    floral *ptr = bk_decorations;
+    floral *end_ptr = bk_decorations + sizeof(bk_decorations) / sizeof(bk_decorations[0]);
+    while (ptr < end_ptr) {
+        draw_floral(ptr);
+        ptr++;
+    }
+
 }
 
 void draw_pointer() {
@@ -139,31 +162,31 @@ void draw_pointer() {
     glEnd();
 }
 
-void draw_floral(floral f) {
+void draw_floral(floral *f) {
     glPushMatrix(); // save previous cs
-    glTranslatef(f.coordinator.x, f.coordinator.y, 0);  // move to new cs
-    glRotatef(f.rotation, 0, 0, 1); // rotate cs
+    glTranslatef(f->coordinator.x, f->coordinator.y, 0);  // move to new cs
+    glRotatef(f->rotation, 0, 0, 1); // rotate cs
 
-    glColor4f(f.color[0], f.color[1], f.color[2], f.alpha);
+    glColor4f(f->color[0], f->color[1], f->color[2], f->alpha);
     use_absolute_cs();
-    switch (f.shape) {
+    switch (f->shape) {
         case floral::SQUARE:
             glBegin(GL_POLYGON);
-            glVertex2f(-f.size, f.size);
-            glVertex2f(f.size, f.size);
-            glVertex2f(f.size, -f.size);
-            glVertex2f(-f.size, -f.size);
+            glVertex2f(-f->size, f->size);
+            glVertex2f(f->size, f->size);
+            glVertex2f(f->size, -f->size);
+            glVertex2f(-f->size, -f->size);
             break;
         case floral::TRIANGLE:
             glBegin(GL_TRIANGLES);
-            glVertex2f(-f.size * std::sqrt(3) / 2, -f.size / 2);
-            glVertex2f(f.size * std::sqrt(3) / 2, -f.size / 2);
-            glVertex2f(0, f.size);
+            glVertex2f(-f->size * std::sqrt(3) / 2, -f->size / 2);
+            glVertex2f(f->size * std::sqrt(3) / 2, -f->size / 2);
+            glVertex2f(0, f->size);
             break;
         case floral::CIRCLE:
             glBegin(GL_POINTS);
             glEnable(GL_POINT_SMOOTH);
-            glPointSize(f.size);
+            glPointSize(f->size);
             glVertex2f(0, 0);
             break;
     }
@@ -184,5 +207,36 @@ void use_percentage_cs() {
     glLoadIdentity();   // reset projection matrix
     gluOrtho2D(0, 1, 1, 0); // use custom coordination system
     glMatrixMode(GL_MODELVIEW);
+}
+
+void init_floral() {
+    floral *ptr = bk_decorations;
+    floral *end_ptr = bk_decorations + sizeof(bk_decorations) / sizeof(bk_decorations[0]);
+    while (ptr < end_ptr) {
+        // random position
+        ptr->coordinator.x = random_int(FLORAL_MARGIN, APP_WIDTH);
+        ptr->coordinator.y = random_int(FLORAL_MARGIN, APP_HEIGHT);
+        // random size
+        ptr->size = random_float(FLORAL_SIZE_MIN, FLORAL_SIZE_MAX);
+        // random rotation
+        ptr->rotation = random_float(-180, 180);
+        // random color
+        ptr->color[0] = random_float(0, 1);
+        ptr->color[1] = random_float(0, 1);
+        ptr->color[2] = random_float(0, 1);
+        // random alpha
+        ptr->alpha = random_float(.2, 1);
+        // random Shape
+        ptr->shape = static_cast<floral::Shape>(rand() % floral::SQUARE);
+        ptr++;
+    }
+}
+
+int random_int(int min, int max) {
+    return rand() % (max - min + 1) + min;
+}
+
+float random_float(float min, float max) {
+    return min + (float) rand() / (float) (RAND_MAX) * max;
 }
 
